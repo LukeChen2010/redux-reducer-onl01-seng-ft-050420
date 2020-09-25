94f68841285cdcf82b16ca964232becbe60da4d8
Luke Chen <luke.chen.2010@gmail.com>
2020-09-25 00:49:27 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,5 +1,6 @@
 export function managePresents(state, action){
   
+<<<<<<< HEAD
   switch(action.type) 
   {
     
@@ -9,4 +10,10 @@ export function managePresents(state, action){
     default:
       return state;
   }
+=======
+  switch(action.type) {
+
+    case "INCREASE":
+      return Object.assign({}, state, {numberOfPresents: state.numberOfPresents + 1});
+>>>>>>> fe419458365f19aa040f50d8a82b0d442197653e
 }
